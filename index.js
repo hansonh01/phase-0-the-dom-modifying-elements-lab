3522fca0d3fac43aa55502484fcc744046c2aa81
Hanson <hanson.hua903@gmail.com>
2023-05-24 00:31:41 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+let newHeader = document.createElement("H1");
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Hanson is the champion";
